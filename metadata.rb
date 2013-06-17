7f2cbdf97beb74ce0828529c4c71df8fc96952e4
Stephen St. Martin <steve@stevestmartin.com>
2013-06-17 13:08:22 -0400

    first commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name 'e2g2'
+description 'E2G2 custom recipe for application setup'
+version '1.0.0'
+maintainer 'Steve St. Martin'
+maintainer_email 'steve.stmartin@e2g2.com'
+license 'Apache 2.0'
+
+depends 'apt'
+
+supports 'ubuntu'
