b493d89506379583e9544cab23fab90ee6410424
Stephen St. Martin <steve@stevestmartin.com>
2013-07-03 10:38:32 -0400

    configuration option for server name

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,11 @@
+# app configuration
 default['app']['name']              = "e2g2"
 default['app']['user']              = "deploy"
 default['app']['user_password']     = "$1$eLsK0ABE$fP/kPLaaqhKaRj8.5lXBL0"
+default['app']['server_name']       = "*.e2g2.dev"
 default['app']['working_directory'] = "/var/www"
+
+# package versions
 default['gdal']['version']          = '1.10.0'
 default['geos']['version']          = '20130627'
 default['nginx']['version']         = '1.4.1'
